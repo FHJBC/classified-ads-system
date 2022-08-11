21b7ef577a1100e04ff32aade509480c938dd0fa
FANOMEZANTSOA Heriniaina Jean Bosco <bosco.hjf@gmail.com>
2022-08-11 15:00:44 +0200

    refactor: make bootstrap and jquery packages working properly with the entire app

--- assets/app.js
+++ assets/app.js
@@ -10,3 +10,19 @@ import './css/app.scss';
 
 // start the Stimulus application
 import './bootstrap';
+
+import $ from 'jquery';
+import 'bootstrap'; // adds functions to jQuery
+
+// const $ = require('jquery');
+// this "modifies" the jquery module: adding behavior to it
+// the bootstrap module doesn't export/return anything
+// require('bootstrap');
+
+// $('.dropdown-toggle').dropdown();
+// $('.custom-file-input').on('change', function (event) {
+//     var inputFile = event.currentTarget;
+//     $(inputFile).parent()
+//         .find('.custom-file-label')
+//         .html(inputFile.files[0].name);
+// });
