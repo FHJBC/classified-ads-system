d3f9590281bcc23d0fa8a5d2ec13bd5fff57620b
FANOMEZANTSOA Heriniaina Jean Bosco <bosco.hjf@gmail.com>
2022-08-11 11:25:25 +0200

    style: add typed.js to create animation for the banner title text

---
+++ assets/js/home_index.js
@@ -0,0 +1,14 @@
+import Typed from "typed.js";
+
+var options = {
+    strings: ['tout ce que vous voulez avec Sera² !', 'des voitures de bonnes occasions.', 'des ordinateurs de bureau ou portable', 'des smartphones'],
+    typeSpeed: 50,
+    backSpeed: 0,
+    backDelay: 500,
+    startDelay: 1000,
+    loop: true,
+    backDelay: 900,
+    backSpeed: 30
+};
+
+var typed = new Typed("#banner-title-slider", options);
